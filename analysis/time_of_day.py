e36aa62d5fd2feaa5e57c94166e8081c1da9407c
Janise Liang <janise@berkeley.edu>
2023-05-11 01:29:55 +0000

    use matching colors for observed and expected in chisquare plot

--- analysis/time_of_day.py
+++ analysis/time_of_day.py
@@ -1,24 +1,22 @@
 """ Functions for time of day analysis """
 
-import pandas as pd
-import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
+import matplotlib.cm as cm
 
 def chisquare(df, cols, plot_expected=True):
-    """ Compare distribution of crash numbers between columns. 
-        
-        Inputs: 
-        
+    """ Compare distribution of crash numbers between columns.
+
+        Inputs:
+
         df (DataFrame): dataframe containing our sample data
         cols (list): list of columns we want to feed into function
-        plot_expected (boolean): if we want to plot the contingency plot 
-        
+        plot_expected (boolean): if we want to plot the contingency plot
+
         Outputs:
         pval (float): p-value for our calculated statistic
-        contributions (DataFrame): shows contributions to test statistic from each cell
-    
+        contributions (DataFrame): contributions to test statistic from each cell
+
     """
     assert len(cols) >= 2
     for c in cols:
@@ -27,11 +25,22 @@ def chisquare(df, cols, plot_expected=True):
     chi2, pval, dof, expected = st.chi2_contingency(contingency)
 
     if plot_expected:
-        plt.plot(expected,
-                 label=[f'Expected {col}' for col in cols],
-                 linestyle='--')
-        plt.plot(contingency,
-                 label=contingency.columns)
+        print(expected.shape, len(expected))
+        colors = cm.tab10.colors
+        for i in range(len(cols)):
+            color = colors[i]
+            exp = expected[:, i]
+            obs = contingency.iloc[:, i]
+            label = contingency.columns[i]
+
+            plt.plot(exp,
+                     label=f'Expected {label}',
+                     linestyle='--',
+                     c=color)
+            plt.plot(obs,
+                     label=label,
+                     c=color)
+
         plt.legend()
         plt.show()
 
